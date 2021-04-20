2cbf43eafc52116dade7f8e7b787df74b2bb0d2b
guilhermeCoutinho <guilhermejcoutinho@gmail.com>
2021-04-20 01:25:26 -0300

    Update user model and migration

--- migrations/20210420020046_create_users_table.go
+++ migrations/20210420020046_create_users_table.go
@@ -8,9 +8,17 @@ import (
 func init() {
 	up := func(db orm.DB) error {
 		_, err := db.Exec(`
+
+		CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 		CREATE TABLE IF NOT EXISTS users (
 			id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-			created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+			user_name VARCHAR NOT NULL,
+			password VARCHAR NOT NULL,
+
+			created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+			updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+			
 		  );
 		  `)
 		return err
--- models/user.go
+++ models/user.go
@@ -8,12 +8,12 @@ import (
 
 //User is the model that governs all notes objects retrived or inserted into the DB
 type User struct {
-	ID       uuid.UUID `json:"_"`
-	UserName string
-	Password string
+	ID       uuid.UUID `json:"_" sql:"id, pk"`
+	UserName string    `sql:"user_name,notnull"`
+	Password string    `sql:"password,notnull"`
 	//	Token         string
 	//	RefreshToken string
 
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `sql:"created_at,notnull"`
+	UpdatedAt time.Time `sql:"updated_at,notnull"`
 }
