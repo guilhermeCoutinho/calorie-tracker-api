a714e26253531c03a5c6f32c03de87c2bafdcff2
guilhermeCoutinho <guilhermejcoutinho@gmail.com>
2021-04-21 16:45:33 -0300

    Add elasticsearch

--- Makefile
+++ Makefile
@@ -1,9 +1,5 @@
 deps:
 	@sh ./dev/deps.sh
-	@make migrate
 
-migrate:
-	@pushd migrations; make migrate; popd
-
-run-server:
+run:
 	@go run *.go serve
--- dev/deps.sh
+++ dev/deps.sh
@@ -9,7 +9,7 @@ echo "=> Starting databases"
 docker-compose \
   --file dev/docker-compose.yaml \
   --project-name=$PROJECT_NAME \
-  up --no-recreate -d postgres redis
+  up --no-recreate -d postgres
 
 until docker exec $PGCONTAINERNAME pg_isready
   do echo "=> Waiting for Postgres..." && sleep 1
@@ -17,4 +17,12 @@ done
 
 docker exec $PGCONTAINERNAME psql -U $PGUSER -d $DBNAME -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
 
+echo "=> Migrating DB"
+
+pushd migrations; make migrate; popd
 echo "=> Starting services"
+
+docker-compose \
+  --file dev/docker-compose.yaml \
+  --project-name=$PROJECT_NAME \
+  up --no-recreate -d
